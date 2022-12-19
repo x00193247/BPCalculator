befb63925dfd69e024cb48ae8d38d44c642aa0e7
RashidMu <x00173247@mytudublin.ie>
2022-12-19 15:29:42 +0000

    Adding Unit test project

---
+++ BPCalculatorUnitTest/BloodPressureUTests.cs
@@ -0,0 +1,16 @@
+namespace BPCalculatorUnitTest
+{
+    public class BloodPressureUTests
+    {
+        //[SetUp]
+        //public void Setup()
+        //{
+        //}
+
+        [Test]
+        public void Tests()
+        {
+            Assert.Pass();
+        }
+    }
+}
